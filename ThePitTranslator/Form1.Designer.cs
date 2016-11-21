9d56275685389bb81f6d8c2740e94ff4f45074ef
Bryce-L <bryce.w.l@gmail.com>
2016-11-20 17:58:24 -0800

    Moving all I have to GitHub.

---
+++ ThePitTranslator/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.inputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.inputRichTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.outputTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(632, 458);
+            this.splitContainer1.SplitterDistance = 276;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // inputRichTextBox
+            // 
+            this.inputRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputRichTextBox.Location = new System.Drawing.Point(0, 0);
+            this.inputRichTextBox.Name = "inputRichTextBox";
+            this.inputRichTextBox.Size = new System.Drawing.Size(632, 276);
+            this.inputRichTextBox.TabIndex = 0;
+            this.inputRichTextBox.Text = "HIUVHEHAARRR HMAUMRRRSH HMOWOI RRRAAQROREHRRRAA OWKHKHNGEHPROWWREHAUNHSH AUF WRMO" +
+    "KHAU THAUWRDERORNH THAANGNGSH, KHRORNHKH PRNGOWMOKH, OWNHHM OW DEEHOWNHWR HMEHWR" +
+    "AUKHAUNHHMRRREHOW.";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(632, 178);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(284, 476);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.goButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 511);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox inputRichTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
---
+++ ThePitTranslator/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private const char escapeCharacter = '"';
+
+        private Dictionary<string, string> translationDictionary = new Dictionary<string, string>
+        {
+            { " ", " "},
+            { ",", ","},
+            { ".", "."},
+            { ":", ":"},
+            { "-", "-"},
+            
+            { "AE", "EE"},
+            { "AUM", "OO"},
+            { "IU", "EA"},
+            { "EO", "OU"},
+            { "KH", "[CH|P]"},
+            { "RT", "TH"},
+            { "TH", "SH"},
+
+            { "AA", "E"},
+            { "AC", "Z"},
+            { "AU", "O"},
+            { "DE", "G"},
+            { "EH", "I"},
+            //{ "F", "F"},
+            { "G", "B"},
+            //{ "H", "H"},
+            { "HM", "[D|M]"},
+            //{ "K", "K"},
+            //{ "HM", "M"},
+            //{ "KH", "P"},
+            { "KR", "J"},
+            { "MOKH", "W"},
+            { "NG", "L"},
+            { "NH", "N"},
+            { "OI", "Y"},
+            { "OW", "A"},
+            { "PR", "C"},
+            { "Q", "Q"},
+            { "ROR", "U"},
+            { "RRR", "R"},
+            { "SH", "S"},
+            { "VH", "V"},
+            { "WR", "T"},
+            //{ "X", "X"},
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            string text = this.inputRichTextBox.Text;
+            this.outputTextBox.Text = Algorithm3(text);
+        }
+
+        /// <summary>           
+        /// Given a string, do the last X character(s) match a key
+        ///     Yes - Replace the longest match, mark the unmatched characters with brackets, and recusively call the method passing it the next character
+        ///     No - Recursively call the method passing it the current string plus the next character.
+        private string Algorithm3(string text)
+        {
+            StringBuilder result = new StringBuilder();
+            string current = String.Empty;
+            for (int x = 0; x < text.Length; x++)
+            {
+                current += text[x];
+
+                // Check for words that I want to provide by wrapping in brackets.
+                if (current.StartsWith(escapeCharacter.ToString()))
+                {
+                    if (current.Length > 1 && current.EndsWith(escapeCharacter.ToString()))
+                    {
+                        result.Append(current.TrimStart(escapeCharacter).TrimEnd(escapeCharacter));
+                        current = String.Empty;
+                    }
+                    continue;
+                }
+
+                string matchingKey = null;
+                for (int y = current.Length - 1; y >= 0; y--)
+                {
+                    string potentialKey = current.Substring(y, current.Length - y);
+                    if (translationDictionary.ContainsKey(potentialKey))
+                    {
+                        matchingKey = potentialKey;
+                    }
+                }
+                if (matchingKey != null)
+                {
+                    if (matchingKey.Length != current.Length)
+                    {
+                        string unmatchedSubString = current.Substring(0, current.Length - matchingKey.Length);
+                        if (unmatchedSubString == "F" || unmatchedSubString == "H" || unmatchedSubString == "K" || unmatchedSubString == "X")
+                        {
+                            result.Append(unmatchedSubString);
+                        }
+                        else
+                        {
+                            result.AppendFormat("[{0}]", unmatchedSubString);
+                        }
+                    }
+                    result.Append(translationDictionary[matchingKey]);
+
+                    current = String.Empty;
+                    matchingKey = null;
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
